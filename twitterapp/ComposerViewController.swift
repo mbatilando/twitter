945271ab694612f3a2152f26b5995c10931769ed
Mari Batilando <mari.batilando@gmail.com>
2015-02-21 19:29:59 -0800

    add outlets and actions

---
+++ twitterapp/ComposerViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ComposerViewController.swift
+//  twitterapp
+//
+//  Created by Mari Batilando on 2/21/15.
+//  Copyright (c) 2015 Mari Batilando. All rights reserved.
+//
+
+import UIKit
+
+class ComposerViewController: UIViewController {
+
+  @IBOutlet weak var profileImageView: UIImageView!
+  @IBOutlet weak var userRealNameLabel: UILabel!
+  @IBOutlet weak var twitterHandleLabel: UILabel!
+  @IBOutlet weak var tweetTextView: UITextView!
+  @IBOutlet weak var charCountLabel: UILabel!
+  
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+  @IBAction func onTweet(sender: AnyObject) {
+  }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
