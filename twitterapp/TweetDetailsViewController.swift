ef23994b2cf40f54f88b8e012ca7936750832ec6
Mari Batilando <mari.batilando@gmail.com>
2015-02-21 20:14:12 -0800

    add ibaction and outlets to tweetdetails

--- twitterapp/TweetDetailsViewController.swift
+++ twitterapp/TweetDetailsViewController.swift
@@ -10,26 +10,33 @@ import UIKit
 
 class TweetDetailsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  @IBOutlet weak var userRealNameLabel: UILabel!
+  
+  @IBOutlet weak var userTwitterHandleLabel: UILabel!
+  @IBOutlet weak var userProfileImageView: UIImageView!
+  @IBOutlet weak var tweetContentLabel: UILabel!
+  @IBOutlet weak var dateTimeLabel: UILabel!
+  @IBOutlet weak var numRetweetsLabel: UILabel!
+  @IBOutlet weak var numFavoritesLabel: UILabel!
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func onRetweet(sender: AnyObject) {
+  }
+  
+  @IBAction func onReply(sender: AnyObject) {
+  }
+  
+  @IBAction func onFavorite(sender: AnyObject) {
+  }
 }
